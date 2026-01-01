a623b0cfa7e86cd977f7443973560b964c0e1695
Alex Viladot <a.viladotgibert@gmail.com>
2026-01-01 15:34:02 +0000

    commit final

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MERN_example/'  // ← ¡IMPORTANTE! Nombre de tu repo
+  base: '/'  
+//base: '/MERN_example/'  // ← ¡IMPORTANTE! Nombre de tu repo
 })
